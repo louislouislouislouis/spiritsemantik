f012042d7c58fd1b43ac1caa4596b3b1f17891b9
louislombard <louis.raphael.lombard@gmail.com>
2021-11-05 09:37:57 +0100

    add homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import fond from "../Assets/img/pexels-koolshooters-8513129.jpg";
 const Home = () => {
+  const [searchValue, setsearchValue] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(searchValue);
+  };
+  const changHandler = (e) => {
+    setsearchValue(e.target.value);
+  };
   return (
     <div className="mainPage">
       <div className="bg">
-        <img src={fond} alt="" />
+        <img src={fond} alt="background" />
       </div>
       <div className="center">
         <div className="title">Choose your Saint</div>
         <div className="formcontainer">
-          <form className="formcontent">
-            <input type="text" />
+          <form onSubmit={submitHandler} className="formcontent">
+            <input type="text" value={searchValue} onChange={changHandler} />
           </form>
         </div>
       </div>
