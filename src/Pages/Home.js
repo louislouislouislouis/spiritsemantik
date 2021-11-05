133f1f7cf2adab2a39d24b017f8c70efbb4401f7
louislombard <louis.raphael.lombard@gmail.com>
2021-11-05 09:27:58 +0100

    add homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import "./Home.css";
+import fond from "../Assets/img/pexels-koolshooters-8513129.jpg";
 const Home = () => {
-  return <div>PAGE HOME</div>;
+  return (
+    <div className="mainPage">
+      <div className="bg">
+        <img src={fond} alt="" />
+      </div>
+      <div className="center">
+        <div className="title">Choose your Saint</div>
+        <div className="formcontainer">
+          <form className="formcontent">
+            <input type="text" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
