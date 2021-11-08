3ff464bb5f9d138de6e2c32b3bd6d6dd63d66c07
louislombard <louis.raphael.lombard@gmail.com>
2021-11-08 08:14:57 +0100

    beggin resultPage

---
+++ src/Component/CardsResult/CardResult.js
@@ -0,0 +1,23 @@
+//React Necessities
+import React from "react";
+
+//Style
+import "./CardResult.css";
+const CardResult = (props) => {
+  return (
+    <div className="cardcontainer">
+      <div className="header">
+        <div className="img">
+          <img src={props.img} alt="" />
+        </div>
+        <div className="title">{props.title}</div>
+      </div>
+      <span className="separator" />
+      <div className="main_div">
+        <p>{props.bio}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CardResult;
--- src/Pages/SearchResults/SearchResult.js
+++ src/Pages/SearchResults/SearchResult.js
@@ -1,5 +1,11 @@
+//React Necessities
 import React from "react";
 import { useParams } from "react-router";
+
+//Custom Component
+import CardResult from "../../Component/CardsResult/CardResult";
+
+//To change
 const DUMMYCARD = [
   {
     bio: "Muhammad Ali — nom couramment retranscrit en français Mohamed Ali ou Mohammed Ali1 — né le 17 janvier 1942 à Louisville au Kentucky et mort le 3 juin 2016 à Scottsdale en Arizona2, est un boxeur américain évoluant en catégorie poids lourds, considéré comme un des plus grands boxeurs de tous les temps...",
@@ -27,7 +33,7 @@ const SearchResult = () => {
   return (
     <div className="results_cards">
       {DUMMYCARD.map((el) => {
-        return <div>{el.bio}</div>;
+        return <CardResult bio={el.bio} title={el.title} img={el.img} />;
       })}
     </div>
   );
